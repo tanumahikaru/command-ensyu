af224281493c43763c200f50748e4b19d20fc70d
tanuma <h.tanuma.sys22@morijyobi.ac.jp>
2022-11-10 10:59:09 +0900

    コミットメッセージを更新

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         
-                System.out.println("身長");
+                System.out.println("一人目身長ｍ入力:");
                 double x=new java.util.Scanner(System.in).nextDouble();
                 System.out.println("体重");
                 double a=new java.util.Scanner(System.in).nextDouble();
                 System.out.printf("%.2f", a/(POW(x)));
+
+                System.out.println("二人目身長ｍ入力:");
+                double y=new java.util.Scanner(System.in).nextDouble();
+                System.out.println("体重");
+                double b=new java.util.Scanner(System.in).nextDouble();
+
+                System.out.printf("一人目"+"%.2f", a/(POW(x)));
+                System.out.printf("二人目"+"%.2f", b/(POW(y)));
             }
                 public static double POW(double n){
                     double ans;
@@ -15,4 +23,3 @@ public class Main {
 
         }
     }
-
